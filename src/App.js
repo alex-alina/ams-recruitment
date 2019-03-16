64f11d7a2e11a0d7a1933da1855a6a5322c45c9b
Alina <alex.alina.rusu@gmail.com>
2019-03-16 17:45:44 +0100

    added and tested mui custom theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#007769',
+    },
+    secondary: {
+      main: '#ccff90',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      'sans-serif',
+    ],
+    // fontSize: 12,
+  },
+}
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hi there</h1>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <CssBaseline />
+          <h1>Hello</h1>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+export default (App);
